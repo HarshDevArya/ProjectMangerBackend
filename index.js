f99df4ea7223da6dc776c5f1d27183071571fe6d
HarshDevArya <harshdevarya96@gmail.com>
2025-05-07 19:53:35 +0530

    vercel Update

--- index.js
+++ index.js
@@ -20,10 +20,17 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://project-manger-front-end.vercel.app",
     credentials: true,
   })
 );
+// app.use(express.json());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 app.use(cookieParser());
 
 /* ----------------------- routes ------------------------ */
