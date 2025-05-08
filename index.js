508f0320fe7817acddaaab7ce9b91fba9eed3b90
HarshDevArya <harshdevarya96@gmail.com>
2025-05-08 21:16:42 +0530

    vercel error resolve

--- index.js
+++ index.js
@@ -17,20 +17,20 @@ const app = express();
 
 /* ---------------------- middleware ---------------------- */
 // app.use(morgan("tiny"));
-// app.use(express.json());
-// app.use(
-//   cors({
-//     origin: "https://project-manger-front-end.vercel.app",
-//     credentials: true,
-//   })
-// );
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://project-manger-front-end.vercel.app",
     credentials: true,
   })
 );
+// app.use(express.json());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 app.use(cookieParser());
 
 /* ----------------------- routes ------------------------ */
